7746b16cf32be9c55bac05922afdf93dd2bdb7e0
mdnavidiqbal <navidiqbal2000@gmail.com>
2025-09-18 23:54:14 +0600

    Add some eventHandler

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country,hanldeVisitedCountry}) => {
+    
+    const [visited,setVisited] = useState(false);
+
 //    console.log(country);
+const handleVisited = () =>{
+    setVisited(visited?false:true);
+    hanldeVisitedCountry(country);
+    
+}
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name : {country.name.common}</h3>
             <p>Population : {country.population.population}</p>
             <p>cca3 : {country.cca3.cca3}</p>
+            <p>Area : {country.area.area}  {country.area.area > 300000 ? "Bigger Country ": "Smaller Country"}</p>
+            <button onClick={handleVisited}>{visited ? 'Visited':'Not Visited'}</button>
         </div>
     );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,16 +1,27 @@
-import { use } from "react"
+import { use, useState } from "react"
 import Country from "../Country/Country";
 import './Countries.css'
 
 export default function Countries({countriesPromise}){
+    const [VisitedCountries,setVisitedCountries] = useState([])
+        const hanldeVisitedCountry = (country) =>{
+            const newVisitedCountry = [...VisitedCountries,country];
+            setVisitedCountries(newVisitedCountry);
+        };
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
     return(
         <div>
             <h1>Total Country : {countries.length}</h1>
+            <h3>Total Visited Country : {VisitedCountries.length}</h3>
+            <ol>
+                {
+                    VisitedCountries.map(country => <li>{country.name.common}</li>)
+                }
+            </ol>
            <div className="countries">
              {
-                countries.map(country => <Country  key={country.cca3.cca3} country = {country}></Country>)
+                countries.map(country => <Country  key={country.cca3.cca3} country = {country} hanldeVisitedCountry = {hanldeVisitedCountry}></Country>)
             }
            </div>
         </div>
